910c815e89dfad99098503094cdd48c9cb54dbc1
lbtt2801 <lebuitantruong@gmail.com>
2023-07-10 20:20:43 +0700

    yukuvn.com

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>YUKU</title>
+
+
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+  <meta name="theme-color" content="#ffffff">
+
+  <style>
+    .font-des {
+      font-family: 'Mali', cursive;
+    }
+    @media screen and (min-width: 413px) {
+      span.font-des {
+        font-size: 50px;
+      }
+      
+      img.img {
+        width: 700px;
+        /* margin-left: 23.5rem !important; */
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    }
+
+    @media screen and (max-width: 412px) {
+      span.font-des {
+        font-size: 30px;
+      }
+      
+      img.img {
+        width: 500px;
+      }
+    }
+    
+    .col-md-4:hover {
+      transform: scale(1.25);
+    }
+  </style>
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&amp;family=Rubik:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Mali:wght@500&display=swap" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main" id="top">
+
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
+      <div class="container"><a class="navbar-brand" href="index.php"><img src="assets/img/gallery/logo.png" width="100%" height="70" alt="logo" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
+        <div style="color:aqua;" class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
+            <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="index.php" style="color:#168445">Trang chủ
+                <hr style="margin-top: 5px; margin-bottom: 0px; color:#168445;" size="4px" width="100%" color="#168445" />
+              </a></li>
+            <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="lythuyet.php" style="color:#168445">Lý thuyết</a></li>
+            <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="luyentap.php" style="color:#168445">Luyện tập</a></li>
+            <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="thithu.php" style="color:#168445">Thi thử</a></li>
+            <li class="nav-item px-2"><a class="nav-link" aria-current="page" href="diendan.php" style="color:#168445">Diễn đàn</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section class="pt-6 bg-600" id="home">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 col-lg-6 order-0 order-md-1 text-end"><img class="body_header pt-7 pt-md-0 w-100" src="assets/img/gallery/body_header.png" width="470" alt="body-header" /></div>
+          <div class="col-md-7 col-lg-6 text-md-start text-center py-6">
+            <span class="font-des" style="color: #133421;">Ôn luyện YUKU</span><br>
+            <span class="font-des" style="color: #133421;">Nắm vững <span style="color: #168445;">ngữ pháp tiếng Trung</span></span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <h1 class="font-des text-center mb-4" style="font-size: 35px; font-weight: 700;"> Hệ thống bài tập ngữ pháp <span style="color:#168445">1000 </span>câu</h1>
+          <img class="img mb-4" src="assets/img/gallery/4chude.jpg" alt="chude">
+          <h1 class="font-des text-center mb-5" style="font-size: 35px; font-weight: 700;"> Các dạng bài tập của YuKu</h1>
+          <div class="col-md-4 mb-4">
+            <img class="card-img-top w-100" src="assets/img/gallery/trac_nghiem.gif" alt="tracnghiem">
+          </div>
+          <div class="col-md-4 mb-4">
+            <img class="card-img-top w-100" src="assets/img/gallery/dung_sai.gif" alt="dungsai">
+          </div>
+          <div class="col-md-4 mb-4">
+            <img class="card-img-top w-100" src="assets/img/gallery/dien_khuyet.gif" alt="dienkhuyet">
+          </div>
+          <div class="col-md-4 mb-4">
+            <img class="card-img-top w-100" src="assets/img/gallery/phan_tang.gif" alt="phantang">
+          </div>
+          <div class="col-md-4 mb-4">
+            <img class="card-img-top w-100" src="assets/img/gallery/phan_loai.gif" alt="phanloai">
+          </div>
+        </div>
+      </div>
+      <!-- end of .container-->
+
+    </section>
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="bg-footer">
+      <div class="container-fluid" style="background-color:#d8e6db">
+        <div class="row text-center  text-me-left">
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h1><img src="assets/img/gallery/logo.png" width="100%" height="100%" alt="logo"> </h1>
+            <p style="color: #133421; text-align: left "> <svg style="background-color: none;" class="svg-inline--fa fa-location-pin fa-w-16 mr-3" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="location-pin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                <g>
+                  <path fill="currentColor" d="M156.5,447.7l-12.6,29.5c-18.7-9.5-35.9-21.2-51.5-34.9l22.7-22.7C127.6,430.5,141.5,440,156.5,447.7z M40.6,272H8.5 c1.4,21.2,5.4,41.7,11.7,61.1L50,321.2C45.1,305.5,41.8,289,40.6,272z M40.6,240c1.4-18.8,5.2-37,11.1-54.1l-29.5-12.6 C14.7,194.3,10,216.7,8.5,240H40.6z M64.3,156.5c7.8-14.9,17.2-28.8,28.1-41.5L69.7,92.3c-13.7,15.6-25.5,32.8-34.9,51.5 L64.3,156.5z M397,419.6c-13.9,12-29.4,22.3-46.1,30.4l11.9,29.8c20.7-9.9,39.8-22.6,56.9-37.6L397,419.6z M115,92.4 c13.9-12,29.4-22.3,46.1-30.4l-11.9-29.8c-20.7,9.9-39.8,22.6-56.8,37.6L115,92.4z M447.7,355.5c-7.8,14.9-17.2,28.8-28.1,41.5 l22.7,22.7c13.7-15.6,25.5-32.9,34.9-51.5L447.7,355.5z M471.4,272c-1.4,18.8-5.2,37-11.1,54.1l29.5,12.6 c7.5-21.1,12.2-43.5,13.6-66.8H471.4z M321.2,462c-15.7,5-32.2,8.2-49.2,9.4v32.1c21.2-1.4,41.7-5.4,61.1-11.7L321.2,462z M240,471.4c-18.8-1.4-37-5.2-54.1-11.1l-12.6,29.5c21.1,7.5,43.5,12.2,66.8,13.6V471.4z M462,190.8c5,15.7,8.2,32.2,9.4,49.2h32.1 c-1.4-21.2-5.4-41.7-11.7-61.1L462,190.8z M92.4,397c-12-13.9-22.3-29.4-30.4-46.1l-29.8,11.9c9.9,20.7,22.6,39.8,37.6,56.9 L92.4,397z M272,40.6c18.8,1.4,36.9,5.2,54.1,11.1l12.6-29.5C317.7,14.7,295.3,10,272,8.5V40.6z M190.8,50 c15.7-5,32.2-8.2,49.2-9.4V8.5c-21.2,1.4-41.7,5.4-61.1,11.7L190.8,50z M442.3,92.3L419.6,115c12,13.9,22.3,29.4,30.5,46.1 l29.8-11.9C470,128.5,457.3,109.4,442.3,92.3z M397,92.4l22.7-22.7c-15.6-13.7-32.8-25.5-51.5-34.9l-12.6,29.5 C370.4,72.1,384.4,81.5,397,92.4z">
+                  </path>
+                  <circle fill="currentColor" cx="256" cy="364" r="28">
+                    <animate attributeType="XML" repeatCount="indefinite" dur="2s" attributeName="r" values="28;14;28;28;14;28;"></animate>
+                    <animate attributeType="XML" repeatCount="indefinite" dur="2s" attributeName="opacity" values="1;0;1;1;0;1;"></animate>
+                  </circle>
+                  <path fill="currentColor" opacity="1" d="M263.7,312h-16c-6.6,0-12-5.4-12-12c0-71,77.4-63.9,77.4-107.8c0-20-17.8-40.2-57.4-40.2c-29.1,0-44.3,9.6-59.2,28.7 c-3.9,5-11.1,6-16.2,2.4l-13.1-9.2c-5.6-3.9-6.9-11.8-2.6-17.2c21.2-27.2,46.4-44.7,91.2-44.7c52.3,0,97.4,29.8,97.4,80.2 c0,67.6-77.4,63.5-77.4,107.8C275.7,306.6,270.3,312,263.7,312z">
+                    <animate attributeType="XML" repeatCount="indefinite" dur="2s" attributeName="opacity" values="1;0;0;0;0;1;"></animate>
+                  </path>
+                  <path fill="currentColor" opacity="0" d="M232.5,134.5l7,168c0.3,6.4,5.6,11.5,12,11.5h9c6.4,0,11.7-5.1,12-11.5l7-168c0.3-6.8-5.2-12.5-12-12.5h-23 C237.7,122,232.2,127.7,232.5,134.5z">
+                    <animate attributeType="XML" repeatCount="indefinite" dur="2s" attributeName="opacity" values="0;0;1;1;0;0;"></animate>
+                  </path>
+                </g>
+              </svg><!-- <i style="background-color:none;" class="fas fa-location-pin mr-3"></i> Font Awesome fontawesome.com -->
+              Khoa tiếng Trung trường Đại học Sư phạm Thành phố Hồ Chí Minh </p>
+
+            <p style="color: #133421; text-align: left "> <svg style="background-color: none;" class="svg-inline--fa fa-mail-bulk fa-w-18 mr-3" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="mail-bulk" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
+                <path fill="currentColor" d="M160 448c-25.6 0-51.2-22.4-64-32-64-44.8-83.2-60.8-96-70.4V480c0 17.67 14.33 32 32 32h256c17.67 0 32-14.33 32-32V345.6c-12.8 9.6-32 25.6-96 70.4-12.8 9.6-38.4 32-64 32zm128-192H32c-17.67 0-32 14.33-32 32v16c25.6 19.2 22.4 19.2 115.2 86.4 9.6 6.4 28.8 25.6 44.8 25.6s35.2-19.2 44.8-22.4c92.8-67.2 89.6-67.2 115.2-86.4V288c0-17.67-14.33-32-32-32zm256-96H224c-17.67 0-32 14.33-32 32v32h96c33.21 0 60.59 25.42 63.71 57.82l.29-.22V416h192c17.67 0 32-14.33 32-32V192c0-17.67-14.33-32-32-32zm-32 128h-64v-64h64v64zm-352-96c0-35.29 28.71-64 64-64h224V32c0-17.67-14.33-32-32-32H96C78.33 0 64 14.33 64 32v192h96v-32z">
+                </path>
+              </svg><!-- <i style="background-color:none;" class="fas fa-mail-bulk mr-3"></i> Font Awesome fontawesome.com -->
+              yuku.nguphaptiengtrungquoc@gmail.com</p>
+          </div>
+          <div class="font-des col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h1 class="font-des text-uppercase mb-4 mt-4 " style="color: #133421; font-size: 20px;">Thông Tin </h1>
+            <p>
+              <a href="about_us.php" style="color: #133421; text-decoration: none;">Về Chúng Tôi</a>
+            </p>
+
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3" style=" color: #ECF634;">
+            <h1 class="font-des text-uppercase mb-4 mt-4 " style="color: #133421; font-size: 20px;">Tài Liệu </h1>
+            <p>
+              <a href="https://www.facebook.com/profile.php?id=100090749690156" style="color: #133421; text-decoration: none">FaceBook</a>
+            </p>
+            <p>
+              <a href="https://gitmind.com/app/docs/m9s578s5" style="color: #133421; text-decoration: none">GitMind</a>
+            </p>
+
+            <p></p>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h1 class="font-des text-uppercase mb-4 mt-4 " style="color: #133421; font-size: 20px;">Cộng Đồng </h1>
+            <p>
+              <a href="https://www.facebook.com/profile.php?id=100090749690156" style="color: #133421; text-decoration: none;">FaceBook</a>
+            </p>
+
+            <p>
+              <a href="https://yukuvn.com" style="color: #133421; text-decoration: none;">WebSite</a>
+            </p>
+            <p>
+              <a href="https://discord.gg/DJBKmjpPEW" style="color: #133421; text-decoration: none;">Discord</a>
+            </p>
+            <!-- Example single danger button -->
+          </div>
+          <div class="container" style="background-color: #93bc9c;">
+            <div class="row justify-content-md-between justify-content-evenly py-4">
+              <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
+                <p class="fs--1 my-2 fw-bold text-white"></p>
+              </div>
+              <div class="col-12 col-sm-8 col-md-6">
+                <p style="font-size: 20px; color: black;"> Bản quyền Ngữ liệu thuộc nhóm NCKH Khoa tiếng Trung trường Đại học Sư phạm Thành phố Hồ Chí Minh
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end of .container-->
+    </section>
+  </main>
+  <!-- ===============================================-->
+  <!--    End of Main Content-->
+  <!-- ===============================================-->
+  <!-- ===============================================-->
+  <!--    JavaScripts-->
+  <!-- ===============================================-->
+  <script src="vendors/@popperjs/popper.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.min.js"></script>
+  <script src="vendors/is/is.min.js"></script>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+  <script src="vendors/fontawesome/all.min.js"></script>
+  <script src="assets/js/theme.js"></script>
+
+  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&amp;family=Rubik:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+
+</body>
+
+</html>
